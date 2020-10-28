0fb337588829819f9b88c69734d25b833211fe88
Bill Shema <shema.bill2@gmail.com>
2020-10-27 22:31:56 -0400

    modified jobcreator.java

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ---
 > We are designing a commucation protocol using java
 ### Group Members
-* [Ariya Rasekh](https://github.com/)
+* [Ariya Rasekh](https://github.com/AriyaRasekh)
 * [Baljot Hansi](https://github.com/bhansi) 
 * [Bill Shema](https://github.com/nohack11)
 * [Christopher Rafinski](https://github.com/brofinski)
--- jobcreator.java
+++ jobcreator.java
@@ -1,4 +1,22 @@
-
+import java.io.*;
+import java.net.*;
 public class jobcreator{
     
+    public static void main(String[] args) {
+        String hostname = "127.0.0.1"; // to be changed later
+        int port = 80;
+
+       try{
+            Socket socket = new Socket(hostname, port);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            socket.close();
+       }
+
+       catch(Exception e){
+           System.out.println(e);
+       }
+
+        
+    }
 }
