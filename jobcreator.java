b3ec82a18f98bfb9fcf407a27777f3696e689687
Bill Shema <shema.bill2@gmail.com>
2020-10-28 10:43:31 -0400

    job creator modified

--- jobcreator.java
+++ jobcreator.java
@@ -4,14 +4,22 @@ import java.net.*;
 public class jobcreator{
     
     public static void main(String[] args) {
-        String hostname = "127.0.0.1"; // to be changed later
         int port = 80;
 
        try{
-            Socket socket = new Socket(hostname, port);
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            socket.close();
+            ServerSocket serversocket = new ServerSocket(port);
+            while (true) {
+                Socket socket = serversocket.accept();
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                String line = reader.readLine();
+                // Sending data
+                OutputStream output = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+                writer.println("Job Creator Message");
+                socket.close();
+                serversocket.close();
+            }  
        }
 
        catch(Exception e){
--- jobseeker.java
+++ jobseeker.java
@@ -4,4 +4,18 @@ import java.net.*;
 // client 
 public class jobseeker{
 
+    public static void main(String args[]){
+
+        String hostname = "127.0.0.1"; // to be changed later
+        int port = 80;
+
+        try {
+            
+            
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println(e);
+        }
+    }
+
 }
