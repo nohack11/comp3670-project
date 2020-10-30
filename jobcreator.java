c447da3a10263c13825e1e125b2cabbed6c94c2d
Bill Shema <shema.bill2@gmail.com>
2020-10-30 19:02:41 -0400

    Adding sending to option to jobcreator

--- jobcreator.java
+++ jobcreator.java
@@ -11,14 +11,27 @@ public class jobcreator{
             while (true) {
                 Socket socket = serversocket.accept();
                 String client = socket.getInetAddress().getHostAddress();
+
+                // Receiving data
                 InputStream input = socket.getInputStream();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-                String line = reader.readLine();
+                
+                String line;
+
+                while(true){
+                    while((line = reader.readLine()) != null){
+                        System.out.println("Client Message: "+line);
+                    }
+                }
+                
+
                 // Sending data
                 OutputStream output = socket.getOutputStream();
                 PrintWriter writer = new PrintWriter(output, true);
                 System.out.println("Client address is: "+client);
                 writer.println("Job Creator Message");
+
+                // 
                 socket.close();
                 serversocket.close();
             }  
