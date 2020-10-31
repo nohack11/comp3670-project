8c4c53b1a23b3adc42f79fc5e07c541c8460d3df
Bill Shema <shema.bill2@gmail.com>
2020-10-30 20:43:57 -0400

    Removed errors on both

--- jobcreator.java
+++ jobcreator.java
@@ -31,7 +31,7 @@ public class jobcreator {
                     OutputStream output = socket.getOutputStream();
                     PrintWriter writer = new PrintWriter(output, true);
                     
-                    writer.println("Job Creator Message");
+                    writer.println("JobCreator Message");
 
                 // 
                     socket.close();
@@ -49,3 +49,68 @@ public class jobcreator {
         
     }
 }
+
+
+// // A Java program for a Server 
+// import java.net.*; 
+// import java.io.*; 
+
+// public class jobcreator 
+// { 
+// 	//initialize socket and input stream 
+// 	private Socket		 socket = null; 
+// 	private ServerSocket server = null; 
+// 	private DataInputStream in	 = null; 
+
+// 	// constructor with port 
+// 	public jobcreator(int port) 
+// 	{ 
+// 		// starts server and waits for a connection 
+// 		try
+// 		{ 
+// 			server = new ServerSocket(port); 
+// 			System.out.println("Server started"); 
+
+// 			System.out.println("Waiting for a client ..."); 
+
+// 			socket = server.accept(); 
+// 			System.out.println("Client accepted"); 
+
+// 			// takes input from the client socket 
+// 			in = new DataInputStream( 
+// 				new BufferedInputStream(socket.getInputStream())); 
+
+// 			String line = ""; 
+
+// 			// reads message from client until "Over" is sent 
+// 			while (!line.equals("Over")) 
+// 			{ 
+// 				try
+// 				{ 
+// 					line = in.readUTF(); 
+// 					System.out.println(line); 
+
+// 				} 
+// 				catch(IOException i) 
+// 				{ 
+// 					System.out.println(i); 
+// 				} 
+// 			} 
+// 			System.out.println("Closing connection"); 
+
+// 			// close connection 
+// 			socket.close(); 
+// 			in.close(); 
+// 		} 
+// 		catch(IOException i) 
+// 		{ 
+// 			System.out.println(i); 
+// 		} 
+// 	} 
+
+// 	public static void main(String args[]) 
+// 	{ 
+// 		jobcreator server = new jobcreator(80); 
+// 	} 
+// } 
+
--- jobseeker.java
+++ jobseeker.java
@@ -1,122 +1,129 @@
-// import java.io.*;
-// import java.net.*;
-// import java.util.Scanner;
-
-// // client 
-// //Ariya is working on it
-// public class jobseeker{
-
-
-
-//     public static void main(String args[]){
-
-//         Scanner sc = new Scanner(System.in);
-//         // initialize socket and input output streams
-//         Socket socket            = null;
-//         //DataInputStream  input   = null;
-//         //DataOutputStream out     = null;
-//         // constructor to put ip address and port
-
-
-//         String hostname = "127.0.0.1"; // to be changed later
-//         int port = 80;
-
-//         InputStream input = null;
-//         try {
-//             socket = new Socket(hostname, port);
-//             System.out.println("Connected");
-
-//             // takes input from terminal
-//             //input = new DataInputStream(System.in);
-//             //BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-
-//             // sends output to the socket
-//             //out = new DataOutputStream(socket.getOutputStream());
-//         }
-
-//         catch(UnknownHostException u) {
-//                 System.out.println(u);
-//         }
-
-//         catch(IOException i) {
-//                 System.out.println(i);
-//         }
-
-//         String line = "";
-// // keep reading until "Over" is input -- Reading from the input
-//         // while (!line.equals("Over"))
-//         // {
-
-
-//         //         line =sc.nextLine();
-//         //         System.out.println(line);
-
-
-//         //     //catch(IOException i)
-//         //     //{
-//         //     //    System.out.println(i);
-//         //     //}
-//         // }
-//         InputStream fromServer = socket.getInputStream();
-//         BufferedReader reader = new BufferedReader(new InputStreamReader(fromServer));
-//         while((line = reader.readLine()) != null){
-//             System.out.println("Client Message: "+line);
-//         }
-// //------------------------------------------------------------------------
-
-// //closing the connection
-//         try
-//         {
-//             //input.close();
-//             //out.close();
-//             socket.close();
-//         }
-//         catch(IOException i)
-//         {
-//             System.out.println(i);
-//         }
-// //------------------------------------------------------------------------------------
-//     }
-
-// }
-
-
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 import java.util.Scanner;
 
-public class jobseeker {
+// client 
+//Ariya is working on it
+public class jobseeker{
 
-    private Socket socket;
-    private Scanner scanner;
 
-    private jobseeker(InetAddress serverAddress, int serverPort) throws Exception {
-        this.socket = new Socket(serverAddress, serverPort);
-        this.scanner = new Scanner(System.in);
-    }
 
-    private void start() throws IOException {
-        String input;
+    public static void main(String args[]){
 
-        while (true) {
-            input = scanner.nextLine();
+        Scanner sc = new Scanner(System.in);
+        // initialize socket and input output streams
+        Socket socket            = null;
+        //DataInputStream  input   = null;
+        //DataOutputStream out     = null;
+        // constructor to put ip address and port
 
-            PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
-            out.println(input);
-            out.flush();
+
+        String hostname = "127.0.0.1"; // to be changed later
+        int port = 80;
+
+        InputStream input = null;
+        try {
+            socket = new Socket(hostname, port);
+            System.out.println("Connected");
+
+            // takes input from terminal
+            
+            input = socket.getInputStream();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+            String line;
+
+            while((line = reader.readLine()) != null){
+                System.out.println("From JobCreator:  "+line);
+            }
+            // sends output to the socket
+            //out = new DataOutputStream(socket.getOutputStream());
         }
+
+        catch(UnknownHostException u) {
+                System.out.println(u);
+        }
+
+        catch(Exception i) {
+                System.out.println(i);
+        }
+
+        String line = "";
+// keep reading until "Over" is input -- Reading from the input
+        // while (!line.equals("Over"))
+        // {
+
+
+        //         line =sc.nextLine();
+        //         System.out.println(line);
+
+
+        //     //catch(IOException i)
+        //     //{
+        //     //    System.out.println(i);
+        //     //}
+        // }
+        InputStream fromServer = socket.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(fromServer));
+        while((line = reader.readLine()) != null){
+            System.out.println("Client Message: "+line);
+        }
+//------------------------------------------------------------------------
+
+//closing the connection
+        try
+        {
+            //input.close();
+            //out.close();
+            socket.close();
+        }
+        catch(IOException i)
+        {
+            System.out.println(i);
+        }
+//------------------------------------------------------------------------------------
     }
-    
-    public static void main(String[] args) throws Exception {
-        jobseeker client = new jobseeker(
-                InetAddress.getByName(args[0]), 
-                Integer.parseInt(args[1]));
-        
-        System.out.println("\r\nConnected to Server: " + client.socket.getInetAddress());
-        client.start();                
-    }
+
 }
+
+
+
+// import java.io.IOException;
+// import java.io.PrintWriter;
+// import java.net.InetAddress;
+// import java.net.Socket;
+// import java.util.Scanner;
+
+// public class jobseeker {
+
+//     private Socket socket;
+//     private Scanner scanner;
+
+//     private jobseeker(InetAddress serverAddress, int serverPort) throws Exception {
+//         this.socket = new Socket(serverAddress, serverPort);
+//         this.scanner = new Scanner(System.in);
+//     }
+
+//     private void start() throws IOException {
+//         String input;
+
+//         while (true) {
+//             input = scanner.nextLine();
+
+//             PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
+//             out.println(input);
+//             out.flush();
+//         }
+//     }
+    
+//     public static void main(String[] args) throws Exception {
+//         jobseeker client = new jobseeker(
+//                 InetAddress.getByName(args[0]), 
+//                 Integer.parseInt(args[1]));
+        
+//         System.out.println("\r\nConnected to Server: " + client.socket.getInetAddress());
+//         client.start();                
+//     }
+// }
 
