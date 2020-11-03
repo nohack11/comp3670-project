67a0046decdf73635385a84e00187f5cde87024c
Bill Shema <shema.bill2@gmail.com>
2020-11-03 10:56:40 -0500

    jobcreator new changes

--- jobcreator.java
+++ jobcreator.java
@@ -1,6 +1,5 @@
 // A Java program for a Server
 import java.net.*;
-<<<<<<< HEAD
 import java.util.*;
 import java.util.concurrent.*;
 
@@ -45,62 +44,6 @@ public class jobcreator {
        catch(Exception e){
            System.out.println(e);
        }
-=======
-import java.io.*;
-
-public class jobcreator
-{
-    //initialize socket and input stream
-    private Socket		 socket = null;
-    private ServerSocket server = null;
-    private DataInputStream in	 = null;
-
-    // constructor with port
-    public jobcreator(int port)
-    {
-        // starts server and waits for a connection
-        try
-        {
-            server = new ServerSocket(port);
-            System.out.println("Server started");
-
-            System.out.println("Waiting for a client ...");
-
-            socket = server.accept();
-            System.out.println("Client accepted");
-
-            // takes input from the client socket
-            in = new DataInputStream(
-                    new BufferedInputStream(socket.getInputStream()));
-
-            String line = "";
-
-            // reads message from client until "Over" is sent
-            while (!line.equals("Over"))
-            {
-                try
-                {
-                    line = in.readUTF();
-                    System.out.println(line);
-
-                }
-                catch(IOException i)
-                {
-                    System.out.println(i);
-                }
-            }
-            System.out.println("Closing connection");
-
-            // close connection
-            socket.close();
-            in.close();
-        }
-        catch(IOException i)
-        {
-            System.out.println(i);
-        }
-    }
->>>>>>> c6d2ac405ce0fb2b9afed081cb9178fed297166b
 
     public static void main(String args[])
     {
