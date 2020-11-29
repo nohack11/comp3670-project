797f074b644a8f0b7e4ffefa4c695e2cfb4765ff
bhansi <baljotshansi@gmail.com>
2020-11-29 16:29:51 -0500

    Started jobOptions foundation. Cleaned code a little.

--- Jobcreator.java
+++ Jobcreator.java
@@ -1,47 +1,45 @@
-
 // A Java program for a Server
-// BH testing push
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-
-
 // This jobcreator Prints out a message to notify connection with jobseeker
 // JobCreator then prints the IP address of JobSeeker
 
 public class Jobcreator {
+    public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int port = 5000;// for peer to peer connection change to port = 61555
+        int port = 5000; // for peer to peer connection change to port = 61555
 
         try {
             ServerSocket serversocket = new ServerSocket(port);
-            Scanner sc = new Scanner(System.in);
 
-            while (true){
+            while(true) {
                 Socket socket = serversocket.accept();
                 String client = socket.getInetAddress().getHostAddress();
-                    // Receiving data
+                // Receiving data
                 InputStream input = socket.getInputStream();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 
-                System.out.println("Job Creator connected");
-                System.out.println("Client address is: "+client);
+                System.out.println("Job Creator connected.");
+                System.out.println("Client address is: " + client);
 
                 //Jobcreator receiving Data from a connected Jobseeker
                 System.out.println("======== MESSAGE RECEIVED ========");
                 String line = reader.readLine();
-                System.out.println("Message from Jobseeker: "+ line);
+                System.out.println("Message from Jobseeker: " + line);
 
                 //JobCreator Sending Data to a connected Jobseeker
-                System.out.println("======== SENDING DATA ===========");
+                System.out.println("========= SENDING DATA ===========");
                 OutputStream output = socket.getOutputStream();
                 PrintWriter writer = new PrintWriter(output, true);
-                writer.println("JobCreator Message");// the message being sent to jobseeker
-                System.out.println("Message sent ...");
+                writer.println("JobCreator Message"); // the message being sent to jobseeker
+                System.out.println("Message sent...");
+
+                System.out.println("You chose: " + jobOptions());
 
                 // JOB ASSIGNMENTS
                 writer.println("Perform your first job\nPrint Job one");
@@ -60,6 +58,13 @@ public class Jobcreator {
        catch(Exception e){
            e.printStackTrace();
        }
+    }
+
+    public static int jobOptions() {
+        System.out.println("What job would you like Jobseeker to perform?");
+        System.out.println("1. Detect if a given IP address or Host Name is online or not.");
+
+        return sc.nextInt();
     }
 }
 
--- Jobseeker.java
+++ Jobseeker.java
@@ -3,23 +3,22 @@
 import java.io.*;
 import java.net.Socket;
 
-//Ariya is working on it
 public class Jobseeker{
 
     public static void main(String[] args) {
 
         String hostname = "127.0.0.1"; // current IP to be changed later
-        int port = 5000;// for peer to peer connection change to port = 61555
+        int port = 5000; // for peer to peer connection change to port = 61555
 
         try {
             Socket socket = new Socket(hostname, port);
-            System.out.println("Connected to Jobcreator");
+            System.out.println("Connected to Jobcreator.");
 
             // Jobseeker sending Data to a connected Jobcreator
             OutputStream out = socket.getOutputStream();
             PrintWriter toServer = new PrintWriter(out, true);
-            System.out.println("======== SENDING DATA  ===========");
-            toServer.println("JobSeeker Message");// Message sent to Jobcreator
+            System.out.println("========= SENDING DATA ===========");
+            toServer.println("JobSeeker Message"); // Message sent to Jobcreator
             System.out.println("Message sent ...");
 
             // Jobseeker receiving Data from a connected Jobcreator
@@ -27,15 +26,15 @@ public class Jobseeker{
             BufferedReader reader = new BufferedReader(new InputStreamReader(input));
             String line = reader.readLine(); // data received
             System.out.println("======== MESSAGE RECEIVED ========");
-            System.out.println("From JobCreator:  "+line+"\n");
+            System.out.println("From JobCreator:  " + line + "\n");
 
             // JOB ASSIGNMENTS
             System.out.println("Waiting for job assignments...");
             String job = reader.readLine();
             System.out.println("Job Assignments:");
-            System.out.println("JOB: "+job);
+            System.out.println("JOB: " + job);
             toServer.println("done");
-            System.out.println("Jobseeker closed");
+            System.out.println("Jobseeker closed.");
             toServer.flush();
         }
         catch(Exception e) {
